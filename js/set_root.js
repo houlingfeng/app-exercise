b70604c1040f05e9c7259f10947426374329ab6d
misshxd <1157847892@qq.com>
2020-03-09 15:27:14 +0800

    app静态页面

---
+++ js/set_root.js
@@ -0,0 +1,13 @@
+
+//设置rem布局的代码。
+(function(){
+	var calc = function(){
+		var docElement = document.documentElement;
+		var clientWidthValue = docElement.clientWidth > 750 ? 750 :docElement.clientWidth;
+		docElement.style.fontSize = 20*(clientWidthValue/375) + 'px';
+	}
+	calc();
+	window.addEventListener('resize',calc);
+
+
+})();
